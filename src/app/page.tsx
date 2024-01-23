26a3a0d8e85c913dc91ae4703cfa4da69179362f
Ejiscoexpress <ejiscoexpress@gmail.com>
2024-01-23 15:41:43 +0100

    Testy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import NewProducts from "./components/NewProducts";
+import Testy from "./components/Testy";
 
 export default function Home() {
   return (
    <main>
     <Hero />
    <NewProducts />
+   <Testy />
    </main>
   );
 }
